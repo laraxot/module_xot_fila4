2f27f099bb65631290ebe6981503dee33707c653
marco76tv <marco.sottana@gmail.com>
2025-09-30 21:29:34 +0200

    .

--- app/Filament/Pages/XotBaseDashboard.php
+++ app/Filament/Pages/XotBaseDashboard.php
@@ -5,7 +5,11 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Pages;
 
+<<<<<<< HEAD
 use Filament\Forms\Form;
+=======
+use Filament\Schemas\Schema;
+>>>>>>> 518e053 (.)
 use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\DatePicker;
 use Filament\Forms\Components\Select;
@@ -21,7 +25,11 @@ abstract class XotBaseDashboard extends FilamentDashboard
 
     final public function filtersForm(Schema $form): Schema
     {
+<<<<<<< HEAD
         return $form->components([
+=======
+        return $form->schema([
+>>>>>>> 518e053 (.)
             Section::make()->schema($this->getFiltersFormSchema())->columns(3),
         ]);
     }

9f19dd236f178086982c8a9c3d7bc09ebb32fe24
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:47:59 +0100

    .

--- app/Filament/Pages/XotBaseDashboard.php
+++ app/Filament/Pages/XotBaseDashboard.php
@@ -4,6 +4,14 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Pages;
 
+<<<<<<< HEAD
+=======
+use Filament\Forms\Form;
+use Filament\Schemas\Components\Section;
+use Filament\Schemas\Components\DatePicker;
+use Filament\Forms\Components\Select;
+use Filament\Schemas\Get;
+>>>>>>> a8f8bb0 (.)
 use Filament\Pages\Dashboard as FilamentDashboard;
 use Filament\Schemas\Components\Component;
 use Filament\Schemas\Components\Section;
@@ -30,13 +38,18 @@ abstract class XotBaseDashboard extends FilamentDashboard
 >>>>>>> 6821f7a (.)
     protected bool $persistsFiltersInSession = true;
 
-    final public function filtersForm(Schema $schema): Schema
+    final public function filtersForm(Schema $form): Schema
     {
+<<<<<<< HEAD
         /** @var array<Component> $filterSchema */
         $filterSchema = $this->getFiltersFormSchema();
 
         return $schema->components([
             Section::make()->schema($filterSchema)->columns(3),
+=======
+        return $form->components([
+            Section::make()->schema($this->getFiltersFormSchema())->columns(3),
+>>>>>>> a8f8bb0 (.)
         ]);
     }
 

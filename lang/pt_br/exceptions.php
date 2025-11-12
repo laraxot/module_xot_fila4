cf933af8644dea07b3dd3ac642cc0b6f5cbf0994
marco76tv <marco.sottana@gmail.com>
2025-11-12 15:47:56 +0100

    .

--- lang/pt_br/exceptions.php
+++ lang/pt_br/exceptions.php
@@ -1,24 +1,17 @@
 <?php
 
 declare(strict_types=1);
-<<<<<<< HEAD
 
-=======
->>>>>>> c06600c (.)
 return [
     /*
 <<<<<<< HEAD
      * |--------------------------------------------------------------------------
      * | Exception Language Lines
-<<<<<<< HEAD
      * |--------------------------------------------------------------------------
-=======
->>>>>>> c06600c (.)
      * |
      * | The following language lines are used in Exceptions thrown throughout the system.
      * | Regardless where it is placed, a button can be listed here so it is easily
      * | found in a intuitive way.
-<<<<<<< HEAD
      * |
      * |--------------------------------------------------------------------------
       |--------------------------------------------------------------------------
@@ -35,9 +28,6 @@ return [
 >>>>>>> f1570712 (.)
      */
 
-=======
-     */
->>>>>>> c06600c (.)
     'backend' => [
         'access' => [
             'roles' => [

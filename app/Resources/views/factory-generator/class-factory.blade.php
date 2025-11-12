77c8347529ce5246c789d10531540346d727c1f0
marco76tv <marco.sottana@gmail.com>
2025-11-12 11:50:37 +0100

    .

--- app/Resources/views/factory-generator/class-factory.blade.php
+++ app/Resources/views/factory-generator/class-factory.blade.php
@@ -2,35 +2,34 @@
 
 declare(strict_types=1);
 
+/** @var \ReflectionClass $reflection */
+/** @var array<string, string> $properties */
+
 ?>
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-@isset($properties['remember_token'])
-    use Illuminate\Support\Str;
-@endisset
-use {{ $reflection->getName() }};
+<?php if (array_key_exists('remember_token', $properties)): ?>
+use Illuminate\Support\Str;
+<?php endif; ?>
+use <?= $reflection->getName(); ?>;
 
-class {{ $reflection->getShortName() }}Factory extends Factory
+class <?= $reflection->getShortName(); ?>Factory extends Factory
 {
-/**
-* The name of the factory's corresponding model.
-*
-* @var string
-*/
-protected $model = {{ $reflection->getShortName() }}::class;
+    /**
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = <?= $reflection->getShortName(); ?>::class;
 
-/**
-* Define the model's default state.
-*
-* @return array
-*/
-public function definition(): array
-{
-return [
-@foreach ($properties as $name => $property)
-    '{{ $name }}' => {!! $property !!},
-@endforeach
-];
-}
+    /**
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+<?php foreach ($properties as $name => $property): ?>
+            '<?= $name ?>' => <?= $property ?>,
+<?php endforeach; ?>
+        ];
+    }
 }

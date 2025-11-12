cbc389d25e596060734368df172a1d4dcdb8a6e1
marco76tv <marco.sottana@gmail.com>
2025-11-12 10:24:15 +0100

    .

--- app/Contracts/UserContract.php
+++ app/Contracts/UserContract.php
@@ -52,7 +52,7 @@ use Spatie\Permission\Exceptions\PermissionDoesNotExist;
  *
  * @phpstan-require-extends Model
  *
- * @mixin \Eloquent
+ * @mixin IdeHelperUserContract
  */
 interface UserContract extends Authenticatable, Authorizable, CanResetPassword, FilamentUser, HasMedia, HasTeamsContract, ModelContract, MustVerifyEmail, PassportHasApiTokensContract
 {

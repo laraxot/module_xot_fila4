4213f5aa0e9d8611a5d5bc0537e9f0eaa82bc259
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:45:19 +0100

    .

--- app/Actions/ModelClass/CountAction.php
+++ app/Actions/ModelClass/CountAction.php
@@ -408,6 +408,7 @@ class CountAction
      */
     public function execute(string $modelClass): int
     {
+        /* @phpstan-ignore-next-line staticMethod.notFound, return.type */
         return InformationSchemaTable::getModelCount($modelClass);
     }
 }
--- tests/Unit/HasTableWithXotTestClass.php
+++ tests/Unit/HasTableWithXotTestClass.php
@@ -46,9 +46,7 @@ class HasTableWithXotTestClass
     public function getLayoutView(): mixed
     {
         $mock = Mockery::mock();
-        /* @phpstan-ignore-next-line method.notFound */
         $mock->shouldReceive('getTableColumns')->andReturn([]);
-        /* @phpstan-ignore-next-line method.notFound */
         $mock->shouldReceive('getTableContentGrid')->andReturn([]);
 
         return $mock;
@@ -62,7 +60,6 @@ class HasTableWithXotTestClass
 
     public function getTable(): Table
     {
-        /* @phpstan-ignore-next-line return.type */
         return Mockery::mock(Table::class);
     }
 

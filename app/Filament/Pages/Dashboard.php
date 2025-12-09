b9d445a50d5379f6384d036eb56a8df90c8ff91f
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:32:12 +0100

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -21,7 +21,11 @@ use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
 =======
 use Filament\Forms\Components\DatePicker;
 use Filament\Schemas\Components\Section;
+<<<<<<< HEAD
 use Filament\Forms\Form;
+=======
+use Filament\Schemas\Schema;
+>>>>>>> 518e053 (.)
 use Filament\Forms\Get;
 use Filament\Pages\Dashboard as BaseBashboard;
 use Filament\Pages\Dashboard\Concerns\HasFiltersForm;

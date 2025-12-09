e34eb3a84d7ed6cf1d0bb5fb80c4fccf4f1fdf65
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:37:19 +0100

    .

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -61,11 +61,7 @@ class Dashboard extends XotBaseDashboard
 >>>>>>> 6cba4fe (.)
 =======
 use Filament\Forms\Components\DatePicker;
-<<<<<<< HEAD
 use Filament\Schemas\Components\Section;
-=======
-use Filament\Forms\Components\Section;
->>>>>>> 2850177 (.)
 use Filament\Schemas\Schema;
 use Filament\Forms\Get;
 use Filament\Pages\Dashboard as BaseBashboard;

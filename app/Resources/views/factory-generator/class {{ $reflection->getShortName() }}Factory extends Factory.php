cc9b1c761e60627c9fef62832a45bcc6def09796
marco76tv <marco.sottana@gmail.com>
2025-12-09 19:53:06 +0100

    .

--- app/Resources/views/factory-generator/class {{ $reflection->getShortName() }}Factory extends Factory.php
+++ app/Resources/views/factory-generator/class {{ $reflection->getShortName() }}Factory extends Factory.php
@@ -259,6 +259,7 @@ declare(strict_types=1);
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 pace Database\Factories;
 =======
 =======
@@ -617,6 +618,10 @@ pace Database\Factories;
 namespace Database\Factories;
 >>>>>>> c84488b (.)
 >>>>>>> 03ceeac3 (.)
+=======
+?>
+namespace Database\Factories;
+>>>>>>> defc17a3 (.)
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 @isset($properties['remember_token'])

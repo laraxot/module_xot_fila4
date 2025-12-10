7c077969af84ee96ad226d83cf93354b96c2ab2b
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:41:13 +0100

    .

--- app/Contracts/UserContract.php
+++ app/Contracts/UserContract.php
@@ -952,7 +952,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsToMany;
  *
  * @phpstan-require-extends Model
  *
- * @mixin \Eloquent
+ * @mixin IdeHelperUserContract
  */
 <<<<<<< HEAD
 <<<<<<< HEAD

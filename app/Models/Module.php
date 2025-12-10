0637d455b2c059aa945d844c2769bbfab09905c0
marco76tv <marco.sottana@gmail.com>
2025-12-10 02:54:39 +0100

    .

--- app/Models/Module.php
+++ app/Models/Module.php
@@ -139,6 +139,7 @@ use Sushi\Sushi;
  * @property bool|null $status
  * @property int|null $priority
  * @property string|null $path
+<<<<<<< HEAD
  * @property string|null $icon
  * @property array<array-key, mixed>|null $colors
  *
@@ -154,6 +155,26 @@ use Sushi\Sushi;
  * @method static Builder<static>|Module wherePriority($value)
  * @method static Builder<static>|Module whereStatus($value)
  *
+=======
+ *
+ * @method static Builder|Module newModelQuery()
+ * @method static Builder|Module newQuery()
+ * @method static Builder|Module query()
+ * @method static Builder|Module whereDescription($value)
+ * @method static Builder|Module whereId($value)
+ * @method static Builder|Module whereName($value)
+ * @method static Builder|Module wherePath($value)
+ * @method static Builder|Module wherePriority($value)
+ * @method static Builder|Module whereStatus($value)
+ *
+ * @property string|null $icon
+ * @property array<string, string>|null $colors
+ *
+ * @method static Builder|Module whereColors($value)
+ * @method static Builder|Module whereIcon($value)
+ *
+ * @mixin IdeHelperModule
+>>>>>>> 713050e (.)
  * @mixin \Eloquent
  */
 final class Module extends BaseModel

afe2f0737a0ac16e199fb455d9e32f5542763fdc
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:45:43 +0100

    .

--- app/Datas/XotData.php
+++ app/Datas/XotData.php
@@ -4545,7 +4545,7 @@ class XotData extends Data implements Wireable
 
     public function save(): void
     {
-        dddx('wip');
+        // dddx('wip'); // Removed debug call
     }
 
     public function getPubThemeViewPath(string $key = ''): string

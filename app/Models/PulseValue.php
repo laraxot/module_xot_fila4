d9a77ff6556d41f72cb934eb1f29e6d2fe5d6099
marco76tv <marco.sottana@gmail.com>
2025-12-10 20:48:52 +0100

    .

--- app/Models/PulseValue.php
+++ app/Models/PulseValue.php
@@ -247,8 +247,8 @@ use Illuminate\Database\Eloquent\Builder;
  * @property string $key
  * @property string $key_hash
  * @property string $value
- * @property-read \Modules\Fixcity\Models\Profile|null $creator
- * @property-read \Modules\Fixcity\Models\Profile|null $updater
+ * @property-read \Modules\Xot\Contracts\ProfileContract|null $creator
+ * @property-read \Modules\Xot\Contracts\ProfileContract|null $updater
  * @method static \Modules\Xot\Database\Factories\PulseValueFactory factory($count = null, $state = [])
  * @method static Builder<static>|PulseValue newModelQuery()
  * @method static Builder<static>|PulseValue newQuery()

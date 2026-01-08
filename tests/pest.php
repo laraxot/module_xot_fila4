b3fb644e1a97d7127de113b61fbba4bc5527b64a
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:59:07 +0100

    .

--- tests/pest.php
+++ tests/pest.php
@@ -322,7 +322,11 @@ use Modules\Xot\Tests\TestCase;
 =======
 >>>>>>> 1c4bb8cf (.)
 
+<<<<<<< HEAD
 pest()->extend(TestCase::class)->in('Feature', 'Unit');
+=======
+uses(TestCase::class)->in('Feature', 'Unit');
+>>>>>>> 54cbe5d (.)
 
 /*
 <<<<<<< HEAD

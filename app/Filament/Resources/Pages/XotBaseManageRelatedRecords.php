c2464342d4197a77f7f77ae93427056f591ee1d6
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:41:46 +0100

    .

--- app/Filament/Resources/Pages/XotBaseManageRelatedRecords.php
+++ app/Filament/Resources/Pages/XotBaseManageRelatedRecords.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace Modules\Xot\Filament\Resources\Pages;
 
-use Filament\Resources\Pages\ManageRelatedRecords as FilamentManageRelatedRecords;
+use Webmozart\Assert\Assert;
+use Illuminate\Database\Eloquent\Model;
 use Modules\Xot\Filament\Traits\HasXotTable;
+use Filament\Resources\Pages\ManageRelatedRecords as FilamentManageRelatedRecords;
 
 /**
  * ---
@@ -13,4 +15,21 @@ use Modules\Xot\Filament\Traits\HasXotTable;
 abstract class XotBaseManageRelatedRecords extends FilamentManageRelatedRecords
 {
     use HasXotTable;
+
+    protected function getDefaultTableSortColumn(): ?string
+    {
+        return null;   
+        /*
+        try {
+            $modelClass = $this->getModelClass();
+            // @var Model $model 
+            $model = app($modelClass);
+            Assert::isInstanceOf($model, Model::class);
+
+            return $model->getTable().'.id';
+        } catch (\Exception $e) {
+            return null;
+        }
+            */
+    }
 }
--- app/Filament/Traits/HasXotTable.php
+++ app/Filament/Traits/HasXotTable.php
@@ -391,6 +391,7 @@ trait HasXotTable
      */
     protected function getDefaultTableSortColumn(): ?string
     {
+        
         try {
             $modelClass = $this->getModelClass();
             /** @var Model $model */

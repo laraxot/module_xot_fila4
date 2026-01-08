41c8cafc076e2e5fc47fb1aa6a3c1c662ef3ec3b
marco76tv <marco.sottana@gmail.com>
2026-01-08 14:47:40 +0100

    .

--- tests/Unit/Console/Commands/GenerateDbDocumentationCommandTest.pest.php
+++ tests/Unit/Console/Commands/GenerateDbDocumentationCommandTest.pest.php
@@ -2,11 +2,12 @@
 
 declare(strict_types=1);
 
+use Modules\Xot\Tests\TestCase;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
 use Modules\Xot\Console\Commands\GenerateDbDocumentationCommand;
 
-uses(\Modules\Xot\Tests\TestCase::class);
+uses(TestCase::class);
 
 beforeEach(function () {
     $this->testSchemaPath = storage_path('tests/schema.json');
